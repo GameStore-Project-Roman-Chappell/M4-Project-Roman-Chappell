095ccbd1db4c2fd6e72283a57245997e59d8cd7a
Roman <alonzofroman@gmail.com>
2022-05-16 09:35:42 -0700

    Added generic exception rest controller advice

--- src/main/java/com/gamestore/gamestore/controller/ControllerExceptionHandler.java
+++ src/main/java/com/gamestore/gamestore/controller/ControllerExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.gamestore.gamestore.controller;
 
+import com.gamestore.gamestore.model.CustomErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -7,5 +10,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(value=Exception.class)
-    public ResponseEntity<>
+    public ResponseEntity<CustomErrorResponse> handleException(Exception e) {
+        CustomErrorResponse error = new CustomErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
+        ResponseEntity<CustomErrorResponse> responseEntity = new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        return responseEntity;
+    }
+
 }
