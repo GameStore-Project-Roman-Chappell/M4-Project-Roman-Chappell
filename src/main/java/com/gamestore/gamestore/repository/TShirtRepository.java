dfccc69f526449fd2be16d5934a1f1a22febb718
adamchappell00 <adam.chappell00@gmail.com>
2022-05-16 08:54:53 -0500

    Initial TShirtRepository Complete

---
+++ src/main/java/com/gamestore/gamestore/repository/TShirtRepository.java
@@ -0,0 +1,11 @@
+package com.gamestore.gamestore.repository;
+
+import com.gamestore.gamestore.model.TShirt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TShirtRepository extends JpaRepository<TShirt, Integer> {
+    List<TShirt> findAllBySize(String size);
+    List<TShirt> findAllByColor(String color);
+}
