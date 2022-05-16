7fcb529beac6e62144413b6d1cb11fe5fa503972
adamchappell00 <adam.chappell00@gmail.com>
2022-05-16 14:10:02 -0500

    Tax and Fee Script Complete

--- src/main/resources/static/tax-fee-seeder.sql
+++ src/main/resources/static/tax-fee-seeder.sql
@@ -1,86 +1,61 @@
 USE game_store;
 
-INSERT INTO sales_tax_rate(tax_rate_id,state, tax)
-VALUES (1,'AL',0.05),
-       (2,'AK',0.06),
-       (3,'AZ',0.04),
-       (4,'AR',0.06),
-       (5,'AL',0.05),
-       (6,'CA',0.06),
-       (7,'CO',0.04),
-       (8,'CT',0.03),
-       (9,'DE',0.05),
-       (10,'FL',0.06),
-       (11,'GA',0.07),
-       (12,'HI',0.05),
-       (13,'ID',0.03),
-       (14,'IL',0.05),
-       (15,'IN',0.05),
-       (16,'IA',0.04),
-       (17,'KS',0.06),
-       (18,'KY',0.04),
-       (19,'LA',0.05),
-       (20,'ME',0.03),
-       (21,'MD',0.07),
-       (1,'MA',0.05),
+# SET PRODOCUT PROCESSING FEES
+INSERT INTO processing_fee(product_type, fee)
+VALUES('console', 14.99),
+       ('tshirt', 1.98),
+       ('game',1.49);
 
+# SET ALL 50 TAX RATES
+INSERT INTO sales_tax_rate(state, tax)
+VALUES ('AL',0.05),
+       ('AK',0.06),
+       ('AZ',0.04),
+       ('AR',0.06),
+       ('CA',0.06),
+       ('CO',0.04),
+       ('CT',0.03),
+       ('DE',0.05),
+       ('FL',0.06),
+       ('GA',0.07),
+       ('HI',0.05),
+       ('ID',0.03),
+       ('IL',0.05),
+       ('IN',0.05),
+       ('IA',0.04),
+       ('KS',0.06),
+       ('KY',0.04),
+       ('LA',0.05),
+       ('ME',0.03),
+       ('MD',0.07),
+       ('MA',0.05),
+       ('MI',0.06),
+       ('MN',0.06),
+       ('MS',0.05),
+       ('MO',0.05),
+       ('MT',0.03),
+       ('NE',0.04),
+       ('NV',0.04),
+       ('NH',0.06),
+       ('NJ',0.05),
+       ('NM',0.05),
+       ('NY',0.06),
+       ('NC',0.05),
+       ('ND',0.05),
+       ('OH',0.04),
+       ('OK',0.04),
+       ('OR',0.07),
+       ('PA',0.06),
+       ('RI',0.06),
+       ('SC',0.06),
+       ('SD',0.06),
+       ('TN',0.05),
+       ('TX',0.03),
+       ('UT',0.04),
+       ('VT',0.07),
+       ('VA',0.06),
+       ('WA',0.05),
+       ('WV',0.05),
+       ('WI',0.03),
+       ('WY',0.04);
 
-Massachusetts—MA: .05
-
-Michigan—MI: .06
-
-Minnesota—MN: .06
-
-Mississippi—MS: .05
-
-Missouri—MO: .05
-
-Montana—MT: .03
-
-Nebraska—NE: .04
-
-Nevada—NV: .04
-
-New Hampshire—NH: .06
-
-New Jersey—NJ: .05
-
-New Mexico—NM: .05
-
-New York—NY: .06
-
-North Carolina—NC: .05
-
-North Dakota—ND: .05
-
-Ohio—OH: .04
-
-Oklahoma—OK: .04
-
-Oregon—OR: .07
-
-Pennsylvania—PA: .06
-
-Rhode Island—RI: .06
-
-South Carolina—SC: .06
-
-South Dakota—SD: .06
-
-Tennessee—TN: .05
-
-Texas—TX: .03
-
-Utah—UT: .04
-
-Vermont—VT: .07
-
-Virginia—VA: .06
-
-Washington—WA: .05
-
-West Virginia—WV: .05
-
-Wisconsin—WI: .03
-
-Wyoming—WY: .04
