ac639e89393a0c2495b601380b96ff74ada74294
adamchappell00 <adam.chappell00@gmail.com>
2022-05-17 12:17:30 -0500

    Added ProductNotFound and Unprocessable Entity to ExceptionHandler

--- src/main/java/com/gamestore/gamestore/controller/ControllerExceptionHandler.java
+++ src/main/java/com/gamestore/gamestore/controller/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.gamestore.gamestore.controller;
 
+import com.gamestore.gamestore.exception.ProductNotFoundException;
+import com.gamestore.gamestore.exception.UnprocessableRequestException;
 import com.gamestore.gamestore.model.CustomErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ControllerExceptionHandler {
 
 
+    @ExceptionHandler(value = UnprocessableRequestException.class)
+    public ResponseEntity<CustomErrorResponse> handleProductNotFoundException(UnprocessableRequestException ure) {
+        CustomErrorResponse error = new CustomErrorResponse(ure.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+        ResponseEntity<CustomErrorResponse> responseEntity = new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+
+    @ExceptionHandler(value = ProductNotFoundException.class)
+    public ResponseEntity<CustomErrorResponse> handleProductNotFoundException(ProductNotFoundException nfe) {
+        CustomErrorResponse error = new CustomErrorResponse(nfe.getMessage(), HttpStatus.NOT_FOUND);
+        ResponseEntity<CustomErrorResponse> responseEntity = new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        return responseEntity;
+    }
 
     @ExceptionHandler(value=Exception.class)
     public ResponseEntity<CustomErrorResponse> handleException(Exception e) {
