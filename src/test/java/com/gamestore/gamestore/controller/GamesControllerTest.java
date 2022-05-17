27ecc071b1a8f992f05c3a771611c35b6f6184d9
Roman <alonzofroman@gmail.com>
2022-05-17 09:15:30 -0700

    Consoles Controller Test added

--- src/test/java/com/gamestore/gamestore/controller/GamesControllerTest.java
+++ src/test/java/com/gamestore/gamestore/controller/GamesControllerTest.java
@@ -1,4 +1,27 @@
 package com.gamestore.gamestore.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gamestore.gamestore.model.Game;
+import com.gamestore.gamestore.repository.GameRepository;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(GamesController.class)
 public class GamesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    GameRepository gamesRepo;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+
 }
